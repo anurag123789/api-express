b01617afc1f958a2a191d08ae124fd9e1aa1b639
Anurag Patel <mail2anuragpatel@gmail.com>
2018-01-27 16:24:01 +0530

    server code

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,21 +1,20 @@
-const mongoose = require('mongoose')
-var Todo = mongoose.model('Todo',{
- text:{
-     type:String,
-     required:true,
-     minlength:1,
-     trim:true
- },
- completed:{
-     type:Boolean,
-     default:false
- },
-    completedAt:{
-       type:Boolean,
-     default:null
-    } 
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
 });
 
-module.exports = {
-    Todo
-}
+module.exports = {Todo};
